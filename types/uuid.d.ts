23fd8e7855f7435a0298b32c5861fe5e047a9145
Ahmad Alhaj Asaad <ahmadalhajasaad1982@gmail.com>
2026-04-29 22:07:33 +0000

    Verwijder uuid type-definities nu de afhankelijkheid is verwijderd

--- types/uuid.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'uuid' {
-  export function v4(): string;
-  export function v1(): string;
-  export function v5(namespace: string | Uint8Array, name: string): string;
-  export function v3(namespace: string | Uint8Array, name: string): string;
-}
